4cded178a27eec9557f404e6b1e3363abddce621
AliKhachab <a.khachab620@gmail.com>
2025-03-22 00:46:12 -0400

    components done

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,7 +1,11 @@
+import Button from "./Button";
+
 export default function AddItemForm() {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <h2>Add an item</h2>
+      <input type="text"/>
+      <Button type="">Add to list</Button>
+    </form>
   )
 }
