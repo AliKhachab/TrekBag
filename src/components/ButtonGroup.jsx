63965b02a65fb902a454185bda8613ccd56d7b32
AliKhachab <a.khachab620@gmail.com>
2025-03-22 01:04:49 -0400

    constants file + formatting

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,15 @@
 import Button from "./Button";
+import { secondaryButtons } from "../lib/constants.js";
 
 export default function ButtonGroup() {
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {
+        secondaryButtons.map(text => {
+          return <Button key={text} type="secondary">{text}</Button>
+        })
+      }
     </section>
   )
 }
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,23 @@
+import { initialItems } from "../lib/constants.js";
+
 export default function ItemList() {
   return (
-    <div>
-      
-    </div>
-  )
+    <ul>
+      {initialItems.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <>
+      <li className="item">
+        <label>
+          <input type="checkbox" checked={item.packed} /> {item.name}
+        </label>
+      </li>
+    </>
+  );
 }
