6e72a51187458373f33bdd9c3b73d0ec4cc86ebe
AliKhachab <a.khachab620@gmail.com>
2025-03-24 13:34:14 -0400

    empty view added, delete and toggle functionality added

---
+++ src/components/EmptyView.jsx
@@ -0,0 +1,8 @@
+export default function EmptyView() {
+  return (
+    <section className="empty-state">
+        <h3>Empty packing list</h3>
+        <p>Start adding some items you absolulely don't want to forget.</p>
+    </section>
+  )
+}
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,22 +1,27 @@
-export default function ItemList( { items } ) {
+import EmptyView from "./EmptyView";
+
+export default function ItemList( { items, handleToggleItem, handleDeleteItem} ) {
   return (
-    <ul>
+    <ul className="item-list">
+      {
+        items.length === 0 && <EmptyView />
+      }
       {items.map((item) => (
-        <Item key={item.id} item={item} />
+        <Item key={item.id} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} item={item} />
       ))}
     </ul>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <>
       <li className="item">
         <label>
-          <input type="checkbox" checked={item.packed} /> {item.name}
+          <input onChange={() => onToggleItem(item.id)} type="checkbox" checked={item.packed} /> {item.name}
         </label>
 
-        <button>❌</button>
+        <button onClick={() => onDeleteItem(item.id)}>❌</button>
       </li>
     </>
   );
